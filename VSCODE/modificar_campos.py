8b3bf76fbb22a183d8235c86458b556f9595f62c
adricev <adriandiez1@gmail.com>
2023-10-31 13:54:12 +0100

    REQUEST DESDE CODIGO A BBDD DE SUPABASE

--- VSCODE/modificar_campos.py
+++ VSCODE/modificar_campos.py
@@ -6,3 +6,8 @@ key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNl
 
 supabase = create_client(url, key)
 
+#para listar los datos de una tabla:
+table_name = input("Introduce el nombre de la tabla a modificar: \n -tag \n -calendars \n -comments \n -events \n -users \n ")
+
+#Hace una consulta de la tabla seleccionada:
+response = supabase.table(table_name).select("*").execute()
